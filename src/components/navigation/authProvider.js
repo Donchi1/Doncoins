27b26a587845771b0a9d363118deb7c38f6628c2
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2021-01-17 23:17:59 +0400

    new commit

--- src/components/navigation/authProvider.js
+++ src/components/navigation/authProvider.js
@@ -36,3 +36,49 @@ export default (type, params) => {
   }
   return Promise.reject('Bad Method')
 }
+
+//import inMemoryJWT from './inMemoryJWT'
+//
+//const authProvider = {
+//  login: ({ username, password }) => {
+//    const request = new Request('http://localhost:5000/authenticate', {
+//      method: 'POST',
+//      body: JSON.stringify({ username, password }),
+//      headers: new Headers({ 'Content-Type': 'application/json' }),
+//      credentials: 'include',
+//      mode: 'no-cors',
+//    })
+//
+//    return fetch(request)
+//      .then((response) => {
+//        if (response.status < 200 || response.status >= 300) {
+//          throw new Error(response.statusText)
+//        }
+//        return response.json()
+//      })
+//      .then(({ token }) => inMemoryJWT.setToken(token))
+//  },
+//
+//  logout: () => {
+//    inMemoryJWT.ereaseToken()
+//    return Promise.resolve()
+//  },
+//
+//  checkAuth: () => {
+//    return inMemoryJWT.getToken() ? Promise.resolve() : Promise.reject()
+//  },
+//
+//  checkError: (error) => {
+//    const status = error.status
+//    if (status === 401 || status === 403) {
+//      inMemoryJWT.ereaseToken()
+//      return Promise.reject()
+//    }
+//    return Promise.resolve()
+//  },
+//  getPermissions: () => {
+//    return inMemoryJWT.getToken() ? Promise.resolve() : Promise.reject()
+//  },
+//}
+//
+//export default authProvider
